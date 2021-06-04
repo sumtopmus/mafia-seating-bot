572bb5d6dbc6385fa1b84950d0b3254a057b2563
Dmitry Bimatov <dbimatov@gmail.com>
2021-06-03 18:29:57 -0700

    UnitTest - use assertEqual(), not assertEquals()

--- test_configuration.py
+++ test_configuration.py
@@ -7,11 +7,11 @@ from configuration import *
 class TestConfiguration(unittest.TestCase):
     def test_configuration_CreateSimple(self):
         c = Configuration(33, 3, 11, 33, 10)
-        self.assertEquals(c.numPlayers, 33)
-        self.assertEquals(c.numTables, 3)
-        self.assertEquals(c.numRounds, 11)
-        self.assertEquals(c.numGames, 33)
-        self.assertEquals(c.numAttempts, 10)
+        self.assertEqual(c.numPlayers, 33)
+        self.assertEqual(c.numTables, 3)
+        self.assertEqual(c.numRounds, 11)
+        self.assertEqual(c.numGames, 33)
+        self.assertEqual(c.numAttempts, 10)
 
     def test_configuration_Readonly(self):
         c = Configuration(30, 3, 10, 30, 10)
