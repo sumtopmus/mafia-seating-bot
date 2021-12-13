1ac2ba87afca387eb61e9aac74edfd97b5e34c7d
Dmitry Bimatov <dbimatov@gmail.com>
2021-12-12 18:52:04 -0800

    Modified print module to support yield strings

--- src/commands.py
+++ src/commands.py
@@ -11,33 +11,38 @@ from optimize_seats import *
 
 from helpers import *
 
-def callbackPrintShortOpponents(s : Schedule):
-    Print.printPairsMatrix(s)
 
-def callbackPrintOpponents(s : Schedule, filename : str):
-    Print.printScheduleByGames(s)
-    Print.printScheduleByPlayers(s)
-    Print.printOpponentsMatrix(s)
-    Print.printPairsMatrix(s)
-    Print.printMinMaxPairs(s, [0, 1])
-    Print.printMinMaxPairs(s, [6, 7, 8, 9])
+def callbackPrintShortOpponents(s: Schedule):
+    Print.print(Print.pairsMatrix(s))
+
+
+def callbackPrintOpponents(s: Schedule, filename: str):
+    Print.print(Print.scheduleByGames(s))
+    Print.print(Print.scheduleByPlayers(s))
+    Print.print(Print.opponentsMatrix(s))
+    Print.print(Print.pairsMatrix(s))
+    Print.print(Print.minMaxPairs(s, [0, 1]))
+    Print.print(Print.minMaxPairs(s, [6, 7, 8, 9]))
 
     if filename is not None:
         saveSchedule(s, filename)
 
-def callbackPrintSeats(s : Schedule, filename : str):
+
+def callbackPrintSeats(s: Schedule, filename: str):
     # Print.printScheduleByGames(s)
-    Print.printSeatsMatrix(s)
+    Print.print(Print.seatsMatrix(s))
 
     if filename is not None:
         saveSchedule(s, filename)
 
-def optimizeOpponents(conf, filename_opponents, numRuns : int, numIterations : int, expectedPairs : list[int] = [0, 0]):
+
+def optimizeOpponents(conf, filename_opponents, numRuns: int, numIterations: int, expectedPairs: list[int] = [0, 0]):
     conf.validate()
-    
+
     solver = OptimizeOpponents(verbose=False)
-    solver.callbackCurrSchedule = lambda s : callbackPrintShortOpponents(s)
-    solver.callbackBetterSchedule = lambda s: callbackPrintOpponents(s, filename_opponents)
+    solver.callbackCurrSchedule = lambda s: callbackPrintShortOpponents(s)
+    solver.callbackBetterSchedule = lambda s: callbackPrintOpponents(
+        s, filename_opponents)
 
     # TODO: refactor into expectedPairs list, -1 means no constraint
     solver.expectedZeroPairs = expectedPairs[0]
@@ -45,7 +50,8 @@ def optimizeOpponents(conf, filename_opponents, numRuns : int, numIterations : i
     s = solver.optimize(conf, numRuns, numIterations)
 
     print("\n*** Schedule after opponents optimization:")
-    callbackPrintOpponents(s, filename_opponents)  
+    callbackPrintOpponents(s, filename_opponents)
+
 
 def optimizeSeats(filename_opponents, filename_seats, numRuns, iterations: list[int]):
     s = loadSchedule(filename_opponents)
@@ -56,13 +62,15 @@ def optimizeSeats(filename_opponents, filename_seats, numRuns, iterations: list[
     callbackPrintSeats(s, None)
 
     seats = OptimizeSeats(s, verbose=False)
-    seats.callbackBetterSchedule = lambda s : callbackPrintSeats(s, filename_seats)
+    seats.callbackBetterSchedule = lambda s: callbackPrintSeats(
+        s, filename_seats)
     seats.optimize(numRuns, iterations)
 
     print("\n*** Schedule after seats optimization:")
     callbackPrintSeats(s, filename_seats)
 
-def generateParticipants(conf : Configuration, filename_participants):
+
+def generateParticipants(conf: Configuration, filename_participants):
     print(f"Creating participants: {conf.numPlayers}")
     p = Participants.create(conf.numPlayers)
     saveParticipants(p, filename_participants)
@@ -75,15 +83,14 @@ def showSchedule(filename, filename_participants):
     if filename_participants is not None:
         participants = loadParticipants(filename_participants)
         s.setParticipants(participants)
-    
-    # Print.printScheduleByGames(s)
-    Print.printScheduleByPlayers(s)
-    Print.printOpponentsMatrix(s)
-    Print.printPairsMatrix(s)
-    Print.printMinMaxPairs(s, [0, 1])
-    Print.printMinMaxPairs(s, [6, 7, 8, 9])
-    Print.printSeatsMatrix(s)
 
-    
+    # Print.printScheduleByGames(s)
+    Print.print(Print.scheduleByPlayers(s))
+    Print.print(Print.opponentsMatrix(s))
+    Print.print(Print.pairsMatrix(s))
+    Print.print(Print.minMaxPairs(s, [0, 1]))
+    Print.print(Print.minMaxPairs(s, [6, 7, 8, 9]))
+    Print.print(Print.seatsMatrix(s))
+
     print("\n*** MWT-compatible schedule:")
-    Print.printMwtSchedule(s)
+    Print.print(Print.mwtSchedule(s))
--- src/print.py
+++ src/print.py
@@ -1,94 +1,107 @@
+from typing import Callable
 from schedule import *
 from metrics import *
 
 
 class Print:
+    @staticmethod
+    def print(func: Callable):
+        for line in func:
+            print(line)
 
     @staticmethod
-    def printSlots(schedule: Schedule):
-        print("\n***Slots:")
+    def slots(schedule: Schedule):
+        yield ''
+        yield "*** Slots:"
         for gameId, slot in schedule.slots.items():
             ids = [player for player in slot.players]
             s = [f"{item:3d}" for item in ids]
             str = ''.join(s)
-            print(f"Slot {gameId:2d}: {str}")
+            yield f"Slot {gameId:2d}: {str}"
 
     @staticmethod
-    def printOpponentsMatrix(schedule: Schedule):
+    def opponentsMatrix(schedule: Schedule):
         m = Metrics(schedule)
         matrix = m.calcOpponentsMatrix()
 
-        print("\n*** Opponents matrix:")
+        yield ''
+        yield "*** Opponents matrix:"
         for playerId in range(len(matrix)):
             line = matrix[playerId]
             s = ''.join([f"{v:3d}" for v in line])
-            print(f"{playerId:2d}: {s}")
+            yield f"{playerId:2d}: {s}"
 
     @staticmethod
-    def printPairsMatrix(schedule : Schedule):
+    def pairsMatrix(schedule: Schedule):
         m = Metrics(schedule)
 
-        print("\nPairs matrix:")
+        yield ''
+        yield "Pairs matrix:"
         for playerId in range(schedule.numPlayers):
             pairs = m.calcPlayerPairsHistogram(playerId)
             penalty = m.penaltyPlayer(playerId)
             header = f"Player {playerId:2d}: err={penalty:6.2f}: "
             str = ''.join([f"{numPairs:4d}" for numPairs in pairs.values()])
-            
-            print(f"{header:25s}{str}")
+
+            yield f"{header:25s}{str}"
 
         # ideal line
         hist = m.penaltyIdealHistogram()
         header = f"*** Ideal: "
         str = ''.join([f"{round(numPairs):4d}" for numPairs in hist.values()])
-        print(f"{header:25s}{str}")
+        yield f"{header:25s}{str}"
 
         # last line
         allPairs = m.calcPairsHistogram()
         header = f"*** Overall: "
         str = ''.join([f"{numPairs:4d}" for numPairs in allPairs.values()])
-        print(f"{header:25s}{str}")
+        yield f"{header:25s}{str}"
 
-    def printMinMaxPairs(schedule : Schedule, numGames: list[int]):
+    def minMaxPairs(schedule: Schedule, numGames: list[int]):
         m = Metrics(schedule)
-        
-        print("\n*** Min-Max opponents")
-        for playerId in range(schedule.numPlayers):         
+
+        yield ''
+        yield "*** Min-Max opponents:"
+        for playerId in range(schedule.numPlayers):
             pairs = m.calcPlayerPairs(playerId)
-            
+
             header = f"Player {playerId:2d}: "
             str = ""
             for idx in numGames:
                 if len(pairs[idx]) == 0:
                     continue
-                s = ''.join([f"{id:2d} " for id in pairs[idx]]) if len(pairs[idx]) < 5 else "..."
+                s = ''.join([f"{id:2d} " for id in pairs[idx]]
+                            ) if len(pairs[idx]) < 5 else "..."
                 str += f"g = {idx} with: [{s}]; "
-            print(header, str)
+            yield f"{header}{str}"
 
     @staticmethod
-    def printPairsHistogram(schedule: Schedule):
+    def pairsHistogram(schedule: Schedule):
         m = Metrics(schedule)
         allPairs = m.calcPairsHistogram()
 
-        print("\nPairs histogram:")
+        yield ''
+        yield "Pairs histogram:"
         for numGames, numPairs in allPairs.items():
             if numPairs > 0:
-                print(f"{numGames:2d} : {numPairs:3d} pairs")
+                yield f"{numGames:2d} : {numPairs:3d} pairs"
 
     @staticmethod
-    def printScheduleByGames(schedule: Schedule):
-        print("\nSchedule by games:")
+    def scheduleByGames(schedule: Schedule):
+        yield ''
+        yield "Schedule by games:"
         for round in schedule.rounds:
-            print(f"\nRound: {round.id}")
+            yield f"\nRound: {round.id}"
             for gameId in round.gameIds:
                 game = schedule.games[gameId]
                 s = [f"{id:3d}" for id in game.players]
                 str = ''.join(s)
-                print(f"Game {game.id:2d}: {str}")
+                yield f"Game {game.id:2d}: {str}"
 
     @staticmethod
-    def printScheduleByPlayers(schedule: Schedule):
-        print("\nSchedule by players:")
+    def scheduleByPlayers(schedule: Schedule):
+        yield ''
+        yield "Schedule by players:"
         for playerId in range(schedule.numPlayers):
             str = ""
             for round in schedule.rounds:
@@ -96,44 +109,46 @@ class Print:
                 for table, gameId in enumerate(round.gameIds):
                     game = schedule.games[gameId]
                     tableStr = chr(ord('A') + table)
-                    for seat,id in enumerate(game.players):
+                    for seat, id in enumerate(game.players):
                         if id == playerId:
                             roundStr = f" {tableStr}:{(seat+1):<2d}"
                             break
                 str += roundStr
 
-            if schedule.participants is None: 
+            if schedule.participants is None:
                 header = f"Player {playerId:2d}: "
             else:
                 playerName = schedule.participants[playerId].name
                 header = f"{playerName:20}:"
-            print(f"{header}{str}")
+            yield f"{header}{str}"
 
     @staticmethod
-    def printSeatsMatrix(schedule: Schedule):
+    def seatsMatrix(schedule: Schedule):
         m = Metrics(schedule)
-
         matrix = m.calcSeatsMatrix()
-        print("\n*** Seats matrix:")
+
+        yield ''
+        yield "*** Seats matrix:"
         for playerId in range(len(matrix)):
             line = matrix[playerId]
             s = ''.join([f"{v:3d}" for v in line])
-            print(f"{playerId:2d}: {s}")
+            yield f"{playerId:2d}: {s}"
 
     @staticmethod
-    def printMwtSchedule(schedule : Schedule):
+    def mwtSchedule(schedule: Schedule):
         for round in schedule.rounds:
             for seat in range(10):
-                ids = [schedule.games[gameId].players[seat] for gameId in round.gameIds]
+                ids = [schedule.games[gameId].players[seat]
+                       for gameId in round.gameIds]
 
                 # output player IDs or player names
                 if not schedule.participants:
                     line = [f"{id:>3d}," for id in ids]
                 else:
-                    line = [f" {schedule.participants.find(id).name}," for id in ids]
-                
+                    line = [
+                        f" {schedule.participants.find(id).name}," for id in ids]
+
                 # print without last comma
                 str = ''.join(line)
-                print(str[:-1])
-            print()
-
+                yield str[:-1]
+            yield ''
