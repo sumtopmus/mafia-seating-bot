ee63f50a2a152f968acd35393e85683350936c68
Dmitry Bimatov <dbimatov@gmail.com>
2021-12-12 21:00:26 -0800

    Fixed unittest - finding module mafia_schedule

--- test/__init__.py
+++ test/__init__.py
@@ -1,11 +1,11 @@
 import sys
 import os
 testdir = os.path.dirname(__file__)
-srcdir = '../src'
+srcdir = '../src/mafia_schedule'
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
 # we turn off formatting here as we need to import src AFTER changing sys.path
 # fmt: off
 import unittest
-import src
+from src.mafia_schedule import *
 # fmt: on
