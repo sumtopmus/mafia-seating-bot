84a3d19b8097e92260ff7aad41bfecec0b58d315
Dmitry Bimatov <dbimatov@gmail.com>
2021-12-30 19:52:00 -0800

    ver 0.0.6

--- test/__init__.py
+++ test/__init__.py
@@ -1,8 +1,15 @@
 import sys
 import os
 testdir = os.path.dirname(__file__)
+
+# add ../src/mafia_schedule
 srcdir = '../src/mafia_schedule'
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+
+# add src/mafia_schedule - may be it helps unit test discover
+srcdir = 'mafia_schedule'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+
 
 # we turn off formatting here as we need to import src AFTER changing sys.path
 # fmt: off
