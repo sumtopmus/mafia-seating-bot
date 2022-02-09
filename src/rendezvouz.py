c4c4f70c2624b84b0c485ffc0b1e918c5b87669f
Dmitry Bimatov <dbimatov@gmail.com>
2022-02-09 13:29:20 -0800

    Rendez-vouz small refactoring and formatting

--- src/rendezvouz.py
+++ src/rendezvouz.py
@@ -2,37 +2,194 @@ from mafia_schedule import *
 from mafia_schedule.helpers import *
 
 
-def createFirstRound(conf: Configuration):
-    games = []
-    game_ids = []
-    for game_id in range(conf.numTables):
-        player_ids = list(range(game_id * 10, (game_id+1) * 10))
-        game = Game(game_id, player_ids)
-        games.append(game)
-        game_ids.append(game.id)
+'''
+Operations: manual set of flips and swaps to create beautiful schedule
+for team Men/Woman tournament.
+Flip: swap team members (m <-> f)
+Swap: swap members of different teams (but same gender!)
+'''
+operations = {
+    0: [],
 
-    round_id = 0
-    round = Round(round_id, game_ids)
+    # flip 0
+    1: [
+        {'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 0},
+        {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 0},
 
-    return round, games
+        # swap before jumping to other gender table
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 1},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 1},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 8},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 8},
+
+    ],
+
+    # flip 1
+    2: [
+        {'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 1},
+        {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 1},
+
+        # swap before jumping to other gender table
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 2},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 2},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 3},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 3},
+
+    ],
+
+    # flip 2
+    3: [
+        {'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 2},
+        {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 2},
+
+        # make for player 4: 3-3 before jumping
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 4},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 4},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 7},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 7},
+
+    ],
+
+    # swap only boys/girls
+    4: [
+        # swap minorities
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 0},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 0},
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 1},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 1},
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 2},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 2},
+    ],
+
+    # flip 3
+    5: [
+        {'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 3},
+        {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 3},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 6},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 6},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 9},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 9},
+
+    ],
+
+    # flip 4
+    6: [
+        {'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 4},
+        {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 4},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 5},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 5},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 8},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 8},
+
+    ],
+
+    # swap in even case1
+    7: [
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 1},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 1},
+
+        # make for player7: play 5-6 before jump
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 7},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 7},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 9},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 9},
+
+    ],
+
+    # swap in even case2
+    8: [{'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 0},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 0},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 3},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 3},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 8},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 8},
+
+        ],
+
+    # flip 5
+    9: [
+        {'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 5},
+        {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 5},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 2},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 2},
+
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 4},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 4},
+    ],
+
+    # flip 6
+    10: [{'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 6},
+         {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 6},
+
+         # swap after  jumping
+         {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 5},
+         {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 5},
+
+         # make for player3: 5-5 after jumping
+         {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 3},
+         {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 3}
+         ],
+
+    # swap before final flip
+    11: [
+        # swap minorities
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 7},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 7},
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 8},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 8},
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 9},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 9},
+    ],
+
+    # flip 7
+    12: [
+        {'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 7},
+        {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 7},
+
+        # swap
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 0},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 0},
+
+        # swap after jumping
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 6},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 6},
+    ],
+
+    # flip 8
+    13: [
+        {'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 8},
+        {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 8},
 
 
-def copyRound(conf: Configuration, all_rounds: Round, all_games: list[Game]):
-    prev_game_id = (len(all_rounds)-1) * conf.numTables
-    start_game_id = len(all_rounds) * conf.numTables
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 1},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 1},
 
-    games = []
-    game_ids = []
-    for game_id in range(conf.numTables):
-        player_ids = all_games[prev_game_id + game_id].players
-        game = Game(start_game_id + game_id, player_ids.copy())
-        games.append(game)
-        game_ids.append(game.id)
+        # swap right after jumping
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 7},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 7},
+    ],
 
-    round_id = len(all_rounds)
-    round = Round(round_id, game_ids)
+    # flip 9
+    14: [
+        {'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 9},
+        {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 9},
 
-    return round, games
+        # swap right after jumping
+        {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 8},
+        {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 8},
+    ],
+}
 
 
 def flipTeamPlayers(conf: Configuration, gameOne: Game, gameTwo: Game, seat: int):
@@ -79,6 +236,39 @@ def execute_operation(operation: dict, conf: Configuration, round: Round, all_ga
         raise RuntimeError("Unknown operation type: {operation['type']")
 
 
+def createFirstRound(conf: Configuration):
+    games = []
+    game_ids = []
+    for game_id in range(conf.numTables):
+        player_ids = list(range(game_id * 10, (game_id+1) * 10))
+        game = Game(game_id, player_ids)
+        games.append(game)
+        game_ids.append(game.id)
+
+    round_id = 0
+    round = Round(round_id, game_ids)
+
+    return round, games
+
+
+def copyRound(conf: Configuration, all_rounds: Round, all_games: list[Game]):
+    prev_game_id = (len(all_rounds)-1) * conf.numTables
+    start_game_id = len(all_rounds) * conf.numTables
+
+    games = []
+    game_ids = []
+    for game_id in range(conf.numTables):
+        player_ids = all_games[prev_game_id + game_id].players
+        game = Game(start_game_id + game_id, player_ids.copy())
+        games.append(game)
+        game_ids.append(game.id)
+
+    round_id = len(all_rounds)
+    round = Round(round_id, game_ids)
+
+    return round, games
+
+
 def createRendezVouzSchedule(conf: Configuration):
     if not conf.isValid():
         return None
@@ -90,190 +280,13 @@ def createRendezVouzSchedule(conf: Configuration):
     all_rounds.append(first_round)
     all_games.extend(first_games)
 
-    operations = {
-        0: [],
-
-        # flip 0
-        1: [{'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 0},
-            {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 0},
-
-            # swap before jumping to other gender table
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 1},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 1},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 8},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 8},
-
-            ],
-
-        # flip 1
-        2: [{'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 1},
-            {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 1},
-
-            # swap before jumping to other gender table
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 2},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 2},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 3},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 3},
-
-            ],
-
-        # flip 2
-        3: [{'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 2},
-            {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 2},
-
-            # make for player 4: 3-3 before jumping
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 4},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 4},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 7},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 7},
-
-            ],
-
-        # swap only boys/girls
-        4: [
-            # swap minorities
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 0},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 0},
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 1},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 1},
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 2},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 2},
-        ],
-
-        # flip 3
-        5: [
-            {'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 3},
-            {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 3},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 6},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 6},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 9},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 9},
-
-        ],
-
-        # flip 4
-        6: [{'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 4},
-            {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 4},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 5},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 5},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 8},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 8},
-
-            ],
-
-        # swap in even case1
-        7: [
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 1},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 1},
-
-            # make for player7: play 5-6 before jump
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 7},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 7},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 9},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 9},
-
-        ],
-
-        # swap in even case2
-        8: [{'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 0},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 0},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 3},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 3},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 8},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 8},
-
-            ],
-
-        # flip 5
-        9: [{'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 5},
-            {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 5},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 2},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 2},
-
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 4},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 4},
-            ],
-
-        # flip 6
-        10: [{'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 6},
-             {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 6},
-
-             # swap after  jumping
-             {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 5},
-             {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 5},
-
-             # make for player3: 5-5 after jumping
-             {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 3},
-             {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 3}
-             ],
-
-        # swap before final flip
-        11: [
-            # swap minorities
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 7},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 7},
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 8},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 8},
-            {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 9},
-            {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 9},
-
-        ],
-
-        # flip 7
-        12: [{'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 7},
-             {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 7},
-
-             # swap
-             {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 0},
-             {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 0},
-
-
-             # swap after jumping
-             {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 6},
-             {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 6},
-             ],
-
-        # flip 8
-        13: [{'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 8},
-             {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 8},
-
-
-             {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 1},
-             {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 1},
-
-             # swap right after jumping
-             {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 7},
-             {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 7},
-             ],
-
-        # flip 9
-        14: [{'type': 'flip', 'table_one': 0, 'table_two': 2, 'seat': 9},
-             {'type': 'flip', 'table_one': 1, 'table_two': 3, 'seat': 9},
-
-             # swap right after jumping
-             {'type': 'swap', 'table_one': 0, 'table_two': 1, 'seat': 8},
-             {'type': 'swap', 'table_one': 2, 'table_two': 3, 'seat': 8},
-
-
-             ],
-    }
-
     for round_id in range(1, conf.numRounds):
+        # copy round from previous one
         round, games = copyRound(conf, all_rounds, all_games)
         all_rounds.append(round)
         all_games.extend(games)
 
+        # apply manual operations
         for op in operations[round_id]:
             execute_operation(op, conf, round, all_games)
 
