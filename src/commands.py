cf18af41f720f360a29f5643581478c1df3dc30e
Dmitry Bimatov <dbimatov@gmail.com>
2022-02-08 16:22:52 -0800

    Commands - use full file names

--- src/commands.py
+++ src/commands.py
@@ -2,11 +2,20 @@ from mafia_schedule import *
 from mafia_schedule.helpers import *
 
 
+def getFilePath(filename: str) -> str:
+    if not filename:
+        return None
+
+    home = os.path.expanduser("~")
+    full_filename = os.path.join(home, filename)
+    return full_filename
+
+
 def callbackPrintShortOpponents(s: Schedule):
     Print.print(Print.pairsMatrix(s))
 
 
-def callbackPrintOpponents(s: Schedule, filename: str):
+def callbackPrintOpponents(s: Schedule, path: str):
     Print.print(Print.scheduleByGames(s))
     Print.print(Print.scheduleByPlayers(s))
     Print.print(Print.opponentsMatrix(s))
@@ -14,25 +23,26 @@ def callbackPrintOpponents(s: Schedule, filename: str):
     Print.print(Print.minMaxPairs(s, [0, 1]))
     Print.print(Print.minMaxPairs(s, [6, 7, 8, 9]))
 
-    if filename is not None:
-        saveSchedule(s, filename)
+    if path is not None:
+        saveSchedule(s, path)
 
 
-def callbackPrintSeats(s: Schedule, filename: str):
+def callbackPrintSeats(s: Schedule, path: str):
     # Print.printScheduleByGames(s)
     Print.print(Print.seatsMatrix(s))
 
-    if filename is not None:
-        saveSchedule(s, filename)
+    if path is not None:
+        saveSchedule(s, path)
 
 
 def optimizeOpponents(conf, filename_opponents, numRuns: int, numIterations: int, expectedPairs: list[int] = [0, 0]):
+    path_opponents = getFilePath(filename_opponents)
     conf.validate()
 
     solver = OptimizeOpponents(verbose=False)
     solver.callbackCurrSchedule = lambda s: callbackPrintShortOpponents(s)
     solver.callbackBetterSchedule = lambda s: callbackPrintOpponents(
-        s, filename_opponents)
+        s, path_opponents)
 
     # TODO: refactor into expectedPairs list, -1 means no constraint
     solver.expectedZeroPairs = expectedPairs[0]
@@ -40,38 +50,44 @@ def optimizeOpponents(conf, filename_opponents, numRuns: int, numIterations: int
     s = solver.optimize(conf, numRuns, numIterations)
 
     print("\n*** Schedule after opponents optimization:")
-    callbackPrintOpponents(s, filename_opponents)
+    callbackPrintOpponents(s, path_opponents)
 
 
 def optimizeSeats(filename_opponents, filename_seats, numRuns, iterations: list[int]):
-    s = loadSchedule(filename_opponents)
+    path_opponents = getFilePath(filename_opponents)
+    path_seats = getFilePath(filename_seats)
+
+    s = loadSchedule(path_opponents)
     s.validate()
     s.generateSlotsFromGames()
 
     # callbackPrintOpponents(s, None)
     callbackPrintSeats(s, None)
-
     seats = OptimizeSeats(s, verbose=False)
     seats.callbackBetterSchedule = lambda s: callbackPrintSeats(
-        s, filename_seats)
+        s, path_seats)
     seats.optimize(numRuns, iterations)
 
     print("\n*** Schedule after seats optimization:")
-    callbackPrintSeats(s, filename_seats)
+    callbackPrintSeats(s, path_seats)
 
 
 def generateParticipants(conf: Configuration, filename_participants):
     print(f"Creating participants: {conf.numPlayers}")
     p = Participants.create(conf.numPlayers)
-    saveParticipants(p, filename_participants)
+
+    path_participants = getFilePath(filename_participants)
+    saveParticipants(p, path_participants)
 
 
 def showSchedule(filename, filename_participants):
-    s = loadSchedule(filename)
+    path_schedule = getFilePath(filename)
+    s = loadSchedule(path_schedule)
     s.validate()
 
     if filename_participants is not None:
-        participants = loadParticipants(filename_participants)
+        path_participants = getFilePath(filename_participants)
+        participants = loadParticipants(path_participants)
         s.setParticipants(participants)
 
     # Print.printScheduleByGames(s)
--- src/mafia_schedule/helpers.py
+++ src/mafia_schedule/helpers.py
@@ -5,33 +5,27 @@ from .schedule import Schedule
 from .player import Player, Participants
 
 
-def saveSchedule(schedule: Schedule, fname: str):
+def saveSchedule(schedule: Schedule, path: str):
     jsonDict = schedule.toJson()
     jsonStr = json.dumps(jsonDict, indent=2)
 
-    home = os.path.expanduser("~")
-    filename = os.path.join(home, fname)
-    print(f"Saving schedule to: {filename}")
-    with open(filename, "w", encoding="utf-8") as f:
+    print(f"Saving schedule to: {path}")
+    with open(path, "w", encoding="utf-8") as f:
         f.write(jsonStr)
 
 
-def saveParticipants(participants: Participants, fname: str):
+def saveParticipants(participants: Participants, path: str):
     jsonDict = participants.toJson()
     jsonStr = json.dumps(jsonDict, indent=2)
 
-    home = os.path.expanduser("~")
-    filename = os.path.join(home, fname)
-    print(f"Saving participants to: {filename}")
-    with open(filename, "w", encoding="utf-8") as f:
+    print(f"Saving participants to: {path}")
+    with open(path, "w", encoding="utf-8") as f:
         f.write(jsonStr)
 
 
-def loadSchedule(fname: str) -> Schedule:
-    home = os.path.expanduser("~")
-    filename = os.path.join(home, fname)
-    print(f"Loading schedule from: {filename}")
-    with open(filename, "r", encoding="utf-8") as f:
+def loadSchedule(path: str) -> Schedule:
+    print(f"Loading schedule from: {path}")
+    with open(path, "r", encoding="utf-8") as f:
         s = f.read()
     d = json.loads(s)
 
@@ -40,11 +34,9 @@ def loadSchedule(fname: str) -> Schedule:
     return s
 
 
-def loadParticipants(fname: str) -> Participants:
-    home = os.path.expanduser("~")
-    filename = os.path.join(home, fname)
-    print(f"Loading participants from: {filename}")
-    with open(filename, "r", encoding="utf-8") as f:
+def loadParticipants(path: str) -> Participants:
+    print(f"Loading participants from: {path}")
+    with open(path, "r", encoding="utf-8") as f:
         s = f.read()
     d = json.loads(s)
 
