338a6e2cadd7b3b3714db6d1b4f9df9f1c4b88cd
Dmitry Bimatov <dbimatov@gmail.com>
2023-02-09 16:35:42 -0500

    Moved configs into separate file

--- src/main.py
+++ src/main.py
@@ -1,89 +1,8 @@
 import sys
 import commands
+import interactive
+from config_dir import Configurations
 from mafia_schedule import Configuration
-
-Configurations = {
-    "VaWaCa-2017":
-        Configuration(numPlayers=25, numTables=2, numRounds=10,
-                      numGames=20, numAttempts=8),
-    "VaWaCa-2019":
-        Configuration(numPlayers=30, numTables=3, numRounds=10,
-                      numGames=30, numAttempts=10),
-    "MiniTournament12":
-        Configuration(numPlayers=12, numTables=1, numRounds=12,
-                      numGames=12, numAttempts=10),
-    "GG-2021":
-        Configuration(numPlayers=36, numTables=3, numRounds=12,
-                      numGames=36, numAttempts=10),
-
-    "VaWaCa-2021":
-        Configuration(numPlayers=36, numTables=3, numRounds=12,
-                      numGames=36, numAttempts=10),
-
-    "mlm2021-20":
-        Configuration(numPlayers=20, numTables=2, numRounds=10,
-                      numGames=20, numAttempts=10),
-
-    "mlm2021-16":
-        Configuration(numPlayers=16, numTables=1, numRounds=16,
-                      numGames=16, numAttempts=10),
-
-    "chicago2021":
-        Configuration(numPlayers=28, numTables=2, numRounds=14,
-                      numGames=28, numAttempts=10),
-
-    "sacramento2021":
-        Configuration(numPlayers=28, numTables=2, numRounds=14,
-                      numGames=28, numAttempts=10),
-
-    "blackfriday2021-20p":
-        Configuration(numPlayers=20, numTables=2, numRounds=10,
-                      numGames=20, numAttempts=10),
-
-    "blackfriday2021-21p":
-        Configuration(numPlayers=21, numTables=2, numRounds=11,
-                      numGames=21, numAttempts=10),
-
-    "rendezvouz-2022":
-        Configuration(numPlayers=40, numTables=4, numRounds=15,
-                      numGames=60, numAttempts=15, numTeams=20),
-
-    "bigapple-2022":
-        Configuration(numPlayers=25, numTables=2, numRounds=13,
-                      numGames=25, numAttempts=10),
-
-    "millenium-2022-30":
-        Configuration(numPlayers=30, numTables=3, numRounds=10,
-                      numGames=30, numAttempts=10),
-
-    "millenium-2022-31":
-        Configuration(numPlayers=31, numTables=3, numRounds=11,
-                      numGames=31, numAttempts=10),
-
-    "millenium-2022-32":
-        Configuration(numPlayers=32, numTables=3, numRounds=11,
-                      numGames=32, numAttempts=10),
-
-    "wedding-2022":
-        Configuration(numPlayers=20, numTables=2, numRounds=8,
-                      numGames=16, numAttempts=8),
-
-    "sacramento-2022-39":
-        Configuration(numPlayers=39, numTables=3, numRounds=13,
-                      numGames=39, numAttempts=10),
-
-    "evergreen-2022":
-        Configuration(numPlayers=20, numTables=2, numRounds=10,
-                      numGames=20, numAttempts=10),
-
-    "blackfriday-2022":
-        Configuration(numPlayers=24, numTables=2, numRounds=12,
-                      numGames=24, numAttempts=10),
-
-    "rv-2023":
-        Configuration(numPlayers=30, numTables=3, numRounds=1,
-                      numGames=3, numAttempts=1),
-}
 
 
 def main():
@@ -106,6 +25,9 @@ def main():
 
     command = sys.argv[1]
     print(f"Command: {command}")
+
+    if command == "interactive":
+        interactive.main_loop(conf)
 
     if command == "opponents":
         filename_opponents = sys.argv[2] if len(
